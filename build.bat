03289182121ea08f40d20c5f7cde708682f9d99e
greUnited <mauichup@proton.me>
2024-07-16 21:01:48 +0100

    Show message box

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-set libs="KERNEL32.LIB" "UCRT.LIB" "USER32.LIB" "legacy_stdio_definitions.lib"
+set libs="KERNEL32.LIB" "UCRT.LIB" "USER32.LIB"
 nasm -f win64 gh_hello.asm -o gh_hello.obj
 link gh_hello.obj /subsystem:console /entry:_main /out:gh_hello.exe %libs%
--- gh_hello.asm
+++ gh_hello.asm
@@ -2,18 +2,24 @@ default rel
 bits 64
 
 section .data
-	enterMsg db "Running...", 0xd, 0xa, 0
+	msgBoxTitle db "Neat title here...", 0
+	msgBoxContent db "Created this nasm assembly (targetting x86-64 Windows 64-bit), or as much as I could with a lack of real system calls on windows, i.e. the 'native system services'", 0
 
 section .text
 	global _main
-	extern printf
+	extern MessageBoxA
 
 _main:
 	push	rbp
 	mov		rbp, rsp
 	sub		rsp, 32
+
+	mov		rcx, 0
+	lea		rdx, [msgBoxContent]
+	lea		r8, [msgBoxTitle]
+	mov		r9, 0
+	call	MessageBoxA
+
 	xor		rax, rax
-	lea		rcx, [enterMsg]
-	call	printf
 	leave
 	ret
